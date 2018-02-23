50ae66556927af562501e722273983f128f0ace6
WInston Ruan <winston.ruan@gmail.com>
2018-02-23 13:44:58 -0500

    real push sorry ian

---
+++ villains-only-sts/src/main/java/com/villains/pojo/Message.java
@@ -0,0 +1,25 @@
+package com.villains.pojo;
+
+public class Message {
+	public String text;
+	
+	public Message() {
+		
+	}
+
+	public Message(String text) {
+		super();
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+
+}
