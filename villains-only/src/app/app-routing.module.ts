8abc86b26434cab7145a24b0c2c930181a8b2dc4
DESKTOP-L2R4GKN\Jonathan <jmoyett590@gmail.com>
2018-02-24 22:14:43 -0500

    Update navbar and router to include profile view

--- villains-only/src/app/app-routing.module.ts
+++ villains-only/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserviewComponent } from './userview/userview.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
   {path: 'homepage', component: HomepageComponent},
-  { path: 'userhome', component: UserviewComponent }
+  { path: 'userhome', component: UserviewComponent },
+  { path: 'userprofile', component: ProfileComponent },
+  { path: 'editprofile', component: EditProfileComponent }
 ];
 
 @NgModule({
