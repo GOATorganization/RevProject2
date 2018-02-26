b68e60cea3a039dcd00903e4024849f9ec375026
DESKTOP-L2R4GKN\Jonathan <jmoyett590@gmail.com>
2018-02-25 22:13:38 -0500

    Modify app router to uninclude edit profile view since its part of profile view

--- villains-only/src/app/app-routing.module.ts
+++ villains-only/src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserviewComponent } from './userview/userview.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ProfileComponent } from './profile/profile.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
   {path: 'homepage', component: HomepageComponent},
   { path: 'userhome', component: UserviewComponent },
   { path: 'userprofile', component: ProfileComponent },
-  { path: 'editprofile', component: EditProfileComponent }
 ];
 
 @NgModule({
