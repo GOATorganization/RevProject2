5b746b6fca52ff4e5828cb7210d82518a6db76be
WInston Ruan <winston.ruan@gmail.com>
2018-03-02 11:24:54 -0500

    Removed passwords from the userview

--- villains-only-sts/src/main/java/com/villains/service/PostServiceImpl.java
+++ villains-only-sts/src/main/java/com/villains/service/PostServiceImpl.java
@@ -44,7 +44,15 @@ public class PostServiceImpl implements PostService {
 
 	@Override
 	public List<Post> getAllPostByUser(User user) {
-		return postRepository.getAllPostByUser(user);
+		List<Post> returner = postRepository.getAllPostByUser(user);
+		for(int i = 0; i < returner.size(); i++) {
+			User rawUser = returner.get(i).getUserId();
+			rawUser.setPassword(null);
+			rawUser.setLikes(null);
+			rawUser.setPosts(null);
+			returner.get(i).setUserId(rawUser);
+		}
+		return returner;
 	}
 
 }
