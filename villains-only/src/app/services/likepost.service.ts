aaae2478b860a6c71a525f4b11ef2140c660b72e
WInston Ruan <winston.ruan@gmail.com>
2018-03-04 21:07:13 -0500

    Removed console logs minor style changes

--- villains-only/src/app/services/likepost.service.ts
+++ villains-only/src/app/services/likepost.service.ts
@@ -18,7 +18,6 @@ export class LikepostService {
 
   likePost(post: Post, user: User) {
     if (!user.likes) {
-      console.log(user.likes);
       user.likes = [];
     }
     user.likes.forEach(element => {
@@ -36,7 +35,6 @@ export class LikepostService {
       user.likes.push(post);
       console.log(user.likes);
     }
-    console.log(post);
     this.userService.addLike(user.likes).subscribe(message => {
 
     },
